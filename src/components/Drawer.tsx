042a5a2fdc3c0d5543bdbfb4de63f7b15f775a2f
Pierre Coetzee <pierre.coetzee@gmail.com>
2025-08-02 10:32:28 +0100

    [P-0] Exclude rubymine

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -45,7 +45,7 @@ export default function Drawer({ children }: DrawerProps) {
 
       {/* Drawer */}
       <div
-        className={`fixed top-0 left-0 h-full w-150 bg-white shadow-xl transform transition-transform duration-300 ease-in-out z-50 ${
+        className={`fixed top-0 left-0 h-full w-200 bg-white shadow-xl transform transition-transform duration-300 ease-in-out z-50 ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
         }`}
       >
